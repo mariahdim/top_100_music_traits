27d4fa01f84284ecbfb81ad58e0b474ad07306f8
mariahdim <mdimalaluan@college.harvard.edu>
2019-12-13 21:52:01 -0500

    completed data analyses, reformatted shiny app, made several data updates

--- top_100_track_data/app.R
+++ top_100_track_data/app.R
@@ -10,49 +10,71 @@ library(forcats)
 library(plotly)
 library(ggthemes)
 library(wordcloud)
+library(tidytext)
+library(ggwordcloud)
 
 # Read the rds file containing the data analyses conducted
 
 df <- read_rds("top100_data.rds")
 
-# Define UI for application. I applied the shinytheme "flatly" for a more
-# minimalistic and sleek design.
+# Define UI for application. I applied the shinytheme "flatly" 
+# for a more minimalistic and sleek design.
 
 ui <- fluidPage(theme = shinytheme("flatly"),
                 
-                navbarPage("What it takes to be a Billboard Hot 100 Song",
+                navbarPage("Can you make it to next year's Billboard Hot 100 Chart?",
                            
                            tabPanel("Overview",
                                     
                                     h2("Objective"),
                                     
-                                    h4("The goal of this project is to determine what qualities encompass a Hot 100 song. I wanted to study how different track characteristics -- like song key, danceability, or energy -- changed over the decades. Using these observations, we might be able to figure out how to create the next Billboard Top 100 song!"),
+                                    h4("The goal of this project is to determine what qualities — both musical and non-musical characteristics — encompass a Hot 100 song. By studying how different track characteristics (e.g., song key, danceability, song energy, etc.) changed over the years, we can gain more insight on how people's song preferences changed overtime. Using these observations, we might be able to figure out how to create the next Billboard Top 100 song!"),
                                     
                                     h2("The Data"),
                                     
-                                    h4("This project used the Billboard Hot 100 charts dating back from 1960 until 2016."),
-                                    
-                                    br(),
-                                    
-                                    h2("Here are some quick snapshots:"),
-                                    
-                                    h4("Most common words in the lyrics of Billboard Hot 100 songs"),
-                                    
-                                    plotOutput("cloud"),
-                                    
-                                    br(),
-                                    
-                                    h4("Artists with the most appearance on the Billboard Hot 100 since 1960"),
-                                    
-                                    DT::dataTableOutput("artists"),
+                                    h4("This project used the Billboard Hot 100 charts dating back from 1960 until 2016. In a separate data set, there is information and scores on different song characteristics. In another data sets, there is information on the song lyrics as well. "),
                                     
                                     br()
                                     
+                                    
                            ),
+                           
+                           # These are just some of the song characteristics
+                           # that are included in the data. These would help me
+                           # figure out how people's music preferences changed
+                           # overtime.
                            
                            tabPanel("Characteristics",
                                     tabsetPanel(
-                                     
+                                        
+                                        tabPanel("Summary",
+                                                 
+                                                 h3(" Overall Trends"),
+                                                 
+                                                 br(),
+                                                 
+                                                 h4("Over time, songs tended to be less acoustic, filled with instrumentals, have the feeling of a live recording, or have low valence scores. On the other hand, more and more songs in the yearly Billboard Hot 100 charts tended to increase in danceability, duration, energy, explicitness, loudness, and speechiness. Overall, the average tempo per year has remained about the same."),
+                                                 
+                                                 h4("An interesting trend is the increase in popularity of danceable and energetic songs over time. This trend seems to be in some conflict with the valence trend, which suggests that more and more songs in the year's top charts, on average, sound more 'negative.'"),
+                                                 br(),
+                                                 
+                                                 h2("Here are some quick snapshots of the data:"),
+                                                 
+                                                 h4("Most common words in the lyrics of Billboard Hot 100 songs"),
+                                                 
+                                                 h5("Unsurprisingly, the word 'love' is the most frequently used word in these songs' lyrics."),
+                                                 
+                                                 plotOutput("cloud"),
+                                                 
+                                                 br(),
+                                                 
+                                                 h4("Artists with the most appearance on the Billboard Hot 100 since 1960"),
+                                                 
+                                                 DT::dataTableOutput("artists"),
+                                                 
+                                                 br()
+                                        ),      
+                                        
                                         tabPanel("Acousticness",
                                                  
                                                  h3("Song Acousticness Trend from 1960 to 2016"),
@@ -63,7 +85,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("acoustic")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Danceability",
                                                  
@@ -85,7 +107,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("duration")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Energy",
                                                  
@@ -95,15 +117,15 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("energy")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Explicit",
                                                  
-                                                 h3("Count of Explicit Songs from 1960 to 2016"),
+                                                 h3("Explicit Songs from 1960 to 2016"),
                                                  
-                                                 h5("Tracks are rated as explicit if they contain explicit lyrics."),
+                                                 h5("A song's explicitness is based on its song lyrics."),
                                                  
-                                                 plotOutput("energy")
+                                                 plotOutput("explicit")
                                                  
                                         ),
                                         
@@ -115,7 +137,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("instrument")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Liveness",
                                                  
@@ -125,7 +147,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("live")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Loudness",
                                                  
@@ -135,7 +157,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("loud")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Speechiness",
                                                  
@@ -145,7 +167,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("speech")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Tempo",
                                                  
@@ -155,7 +177,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("tempo")
                                                  
-                                                 ),
+                                        ),
                                         
                                         tabPanel("Valence",
                                                  
@@ -165,7 +187,7 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  plotOutput("valence")
                                                  
-                                                 )
+                                        )
                                     )
                            ),
                            
@@ -198,14 +220,18 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  h5("After looking at each individual song key, there appeared to be no strong trend indicating a shift in song key preference. The C and G are the most popular song keys on the list.")
                                                  
                                         ),
-                                    
+                                        
                                         tabPanel("Mode",
                                                  
                                                  h3("Major vs. Minor Key"),
                                                  
                                                  h5("Mode indicates the modality (major or minor) of a track, the type of scale from which its melodic content is derived."),
                                                  
-                                                 plotOutput("mode")
+                                                 plotOutput("mode"),
+                                                 
+                                                 br(),
+                                                 
+                                                 h5("Overtime, more and more songs that make it to the Top 100 charts are written in the minor key.")
                                                  
                                         ),
                                         
@@ -213,7 +239,15 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                                  
                                                  h3("How frequently did each chord appear on the list?"),
                                                  
-                                                 plotOutput("chord_freq")
+                                                 plotOutput("chord_freq"),
+                                                 
+                                                 br(),
+                                                 
+                                                 h3("General Observations"),
+                                                 
+                                                 h5("In the chart above, the song keys C and G still were the most popular song keys over all. However, upon closer inspection, it seemed like those keys are not as popular in recent years. As a whole, it seemed like song keys have no major trends that are pointing to any music preference changes."),
+                                                 
+                                                 h5("Interestingly, the mode that a song key is in seems like there might be some preference changes overtime. In 1969, less than 15% of the songs on the chart were written in the minor key. In 2015, this number has increased to almost 40%. Song in minor keys tend to be perceived as more melancholy. The increase in popularity of songs in minor keys may indicate an increase in popularity of sad music.")
                                         )
                                     )
                            ),
@@ -224,8 +258,12 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                         
                                         h3("Traits"),
                                         
+                                        h5("Instructions: Use the slider to input your song's characteristics to determine how well it might do on the Billboard Top 100 list!"),
+                                        
                                         sliderInput("input_key", "Key", min = 0, max = 11, value = 0),
-                                        sliderInput("input_mode", "Major Key", min = 0, max = 1, value = 0, step = 1),
+                                        h6("0 = C, 1 = C#/Db, 2 = D, 3 = D#/Eb, 4 = E, 5 = F, 6 = F#/Gb, 7 = G, 8 = G#/Ab, 9 = A, 10 = A#/Bb, 11 = B"),
+                                        sliderInput("input_mode", "Mode (Major Key)", min = 0, max = 1, value = 0, step = 1),
+                                        h6("A '1' indicates a major key."),
                                         sliderInput("input_danceability", "Danceability", min = 0, max = 1, value = 0),
                                         sliderInput("input_energy", "Energy", min = 0, max = 1, value = 0),
                                         sliderInput("input_loudness", "Loudness (dB)", min = -60, max = 0, value = 0),
@@ -236,11 +274,11 @@ ui <- fluidPage(theme = shinytheme("flatly"),
                                         sliderInput("input_valence", "Valence", min = 0, max = 1, value = 0)
                                     ),
                                     
-                                    mainPanel(
+                                    sidebarPanel(
                                         
-                                        h3("The predicted Billboard Hot 100 ranking for those characteristics is:"),
+                                        h3("Your predicted ranking is:"),
                                         
-                                        h2(textOutput("predictor"))
+                                        h1(textOutput("predictor"))
                                     )
                            ),
                            
@@ -350,7 +388,13 @@ server <- function(input, output) {
         
         tidy_model <- df$tidy_model
         
-        round(tidy_model$`(Intercept)` + tidy_model$key * input$input_key + tidy_model$mode * input$input_mode + tidy_model$acousticness * input$input_acousticness + tidy_model$danceability * input$input_danceability + tidy_model$energy * input$input_energy + tidy_model$loudness * input$input_loudness + tidy_model$speechiness * input$input_speechiness + tidy_model$instrumentalness * input$input_instrumentalness + tidy_model$liveness * input$input_liveness + tidy_model$valence * input$input_valence)
+        rank = round(tidy_model$`(Intercept)` + tidy_model$key * input$input_key + tidy_model$mode * input$input_mode + tidy_model$acousticness * input$input_acousticness + tidy_model$danceability * input$input_danceability + tidy_model$energy * input$input_energy + tidy_model$loudness * input$input_loudness + tidy_model$speechiness * input$input_speechiness + tidy_model$instrumentalness * input$input_instrumentalness + tidy_model$liveness * input$input_liveness + tidy_model$valence * input$input_valence)
+        
+        if (rank <= 0) {
+            rank = 1
+        }
+        
+        rank
     })
     
     output$predictor <- renderText({
